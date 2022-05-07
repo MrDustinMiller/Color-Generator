eb9caff60f0d419f6015187d3be957c3399ff249
MrDustinMiller <97045700+MrDustinMiller@users.noreply.github.com>
2022-05-07 13:43:32 -0500

    Update Program.cs

--- Color Generator/Color Generator/Program.cs
+++ Color Generator/Color Generator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace DevinColors
 {
@@ -11,14 +11,13 @@ namespace DevinColors
                                 "Dark Blue", "Light Green", "Deep Black", "Bright Red", "Turquoise", "Grey", "Magenta", "Air Force Blue"};
 
             //call function
-            UserColors(colors);
+            PromptInput(colors);
         }//end main
 
-        static void UserColors(string[] colors)
+        static void PromptInput(string[] colors)
         {
             //vars
             int userInput;
-            int count = -1;
             char y = 'y';
             char n = 'n';
 
@@ -26,9 +25,36 @@ namespace DevinColors
             Console.Write("How many colors do you want to choose today? ");
             bool success = int.TryParse(Console.ReadLine(), out userInput);
 
+            CreateRandomGenerator(userInput, colors);
+
+            //ask user if they would like to generate another set of colors
+            Console.WriteLine("\nGo one more time? y/n");
+
+            //read the key press
+            ConsoleKeyInfo info = Console.ReadKey();
+
+            if (info.KeyChar == y) //then
+            {
+                Console.WriteLine();
+                ContinueGenerator(colors);
+            }//end if
+
+            else if (info.KeyChar == n) //then
+            {
+                Console.WriteLine("\nGoodbye! :) ");
+            }//end else if
+           
+        }//end function
+        
+        static void CreateRandomGenerator(int userInput, string[] colors)
+        {
             //create random number generator
             Random numberGen = new Random();
 
+            GenerateColors(userInput, colors, numberGen);
+        }
+        static void GenerateColors(int userInput, string[] colors, Random numberGen)
+        {
             //display setup
             Console.Write("Your colors are: ");
 
@@ -43,49 +69,13 @@ namespace DevinColors
             }//end for
 
             Console.WriteLine();
-
-            //ask user if they would like to generate another set of colors
-            Console.WriteLine("\nGo one more time? y/n ");
-
-            //read the key press
-            ConsoleKeyInfo info = Console.ReadKey();
-
-            while (count < 0)
-            {
-                if (info.KeyChar == y) //then
-                {
-                    //disply new set statement
-                    Console.WriteLine("\n");
-                    Console.Write("Your new set of colors is: ");
-
-                    //for loop to run through array one more time
-                    for (int i = 0; i < userInput; i++)
-                    {
-                        colors[i] = colors[numberGen.Next(0, colors.Length)];
-
-                        //display new set of colors
-                        Console.Write("{0} ,", colors[i]);
-
-                        //increment count
-                        count++;
-
-                        //break out of loop
-                        if (count > 50)
-                        {
-                            break;
-                        }
-                    }//end for
-                }
-
-                //if key press is n then exit and break out of the while loop
-                if (info.KeyChar == n)
-                {
-                    Console.WriteLine("\nGoodbye! :) ");
-                    break;
-                }//end if
-
-            }//end while
-        } //end function
+        }
+        
+        static void ContinueGenerator(string[] colors)
+        {         
+             PromptInput(colors);   
+            
+        }//end function
 
     }//end class
 }//end namespace
